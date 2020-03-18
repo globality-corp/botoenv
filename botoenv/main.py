90a8f8190c8c308c6caf2eb31c8ffcb115e4d206
Andre Choi <andre.choi@globality.com>
2020-03-17 18:24:23 -0700

    feature/DEVOPS-1066-fix_bug_on_last_botoenv_commit

--- botoenv/main.py
+++ botoenv/main.py
@@ -24,7 +24,7 @@ def build_credentials_map(session, credentials):
     if "AWS_DEFAULT_REGION" in environ:
         del environ["AWS_DEFAULT_REGION"]
     else:
-        environ["AWS_DEFAULT_REGION"]=session.get_config_variable("region")
+        environ["AWS_DEFAULT_REGION"] = session.get_config_variable("region")
 
     if credentials.token is None:
         return dict(
